38d38531ffd43cf65b1ed8735ef87e882c554b76
exing1 <71097400+exing1@users.noreply.github.com>
2021-01-23 21:27:56 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core';
+import { cyan, deepOrange } from '@material-ui/core/colors';
 import Dashboard from './components/Dashboard';
+
+
+const theme = createMuiTheme(
+  {
+    palette: {
+      primary: {
+        main: "#E0E4E8"
+      },
+      secondary: {
+        main: deepOrange[100]
+      },
+    },
+  }
+);
 
 function App() {
   return (
